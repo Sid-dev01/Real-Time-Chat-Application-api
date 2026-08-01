640f4820fd0ed103efd77ea9cae0ccd2456adabf
Siddhartha Gautam <sid.2005.19@gmail.com>
2026-08-01 17:22:35 +0530

    Completed repository code.

--- src/db/drizzle.service.ts
+++ src/db/drizzle.service.ts
@@ -1,17 +1,25 @@
 import postgres from 'postgres';
+import * as schema from './schema';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { drizzle } from 'drizzle-orm/postgres-js';
+import { drizzle, type PostgresJsDatabase } from 'drizzle-orm/postgres-js';
 
 @Injectable()
 export class DrizzleService {
-    readonly db;
+    private readonly db: PostgresJsDatabase<typeof schema>;
 
     constructor(private readonly configService: ConfigService) {
         const client = postgres(
-            this.configService.getOrThrow<string>('app.databaseUrl')
+            this.configService.getOrThrow<string>('app.databaseUrl'),
+            {
+                max: 10
+            }
         )
-        this.db = drizzle(client);
+        this.db = drizzle(client, { schema});
+    }
+
+    get database(): PostgresJsDatabase<typeof schema> {
+        return this.db;
     }
 
 }
